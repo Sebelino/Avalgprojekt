ea4ea38c62883f345e482a44ff8d7d20977399a0
Sebastian Olsson <sebelino@hotmail.com>
2013-10-09 20:01:36 +0200

    Lagt till stöd för "java Run Pollard < input/input1"-syntax.

--- ecklim/src/Run.java
+++ ecklim/src/Run.java
@@ -1,14 +1,13 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
 import java.math.BigInteger;
 import java.util.List;
-import java.util.ArrayList;
 
 public class Run{
 	public static void main(String[] args){
 
-		Factorizer factorizer = new NaiveEratost2();
+		Factorizer factorizer = getFactorizer(args);
+        if(factorizer == null){
+            factorizer = new NaiveEratost2();
+        }
 		List<BigInteger> input = Util.read();
 		for(BigInteger n : input){
 			List<BigInteger> factorization = factorizer.factorize(n);
@@ -24,10 +23,32 @@ public class Run{
 					Util.printFactorization(factorization);
 				}
 				else {
-					int a = 1/0;  // Kattis debug, will give run time error if we get here
+					int a = 1/0;  // Kattis debug; will give run time error if we get here
 					Util.printFactorization(null);
 				}
 			}
 		}
 	}
+
+    @SuppressWarnings("rawtypes")
+    private static Factorizer getFactorizer(String[] args){
+        if(args.length == 1){
+            String token = args[0];
+            Class c = null;
+            try{
+                c = Class.forName(token);
+            }catch(ClassNotFoundException e){
+                System.err.println("Class "+token+" not found. Running default algorithm.");
+                return null;
+            }
+            try{
+                return (Factorizer)c.newInstance();
+            }catch(InstantiationException e){
+                System.err.println("The class could not be instantiated.");
+            }catch(IllegalAccessException e){
+                System.err.println("Illegal access.");
+            }
+        }
+        return null;
+    }
 }
